59536c50a87390854a12a0292088f97af1803c6d
Nikita Makhotin <makhotin.07@gmail.com>
2022-10-24 12:23:15 +0300

    2 commit

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from posts.models import Comment, Group, Post
 
 
-
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
