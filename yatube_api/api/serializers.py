b13b97fe20e55328b40c3e8adb12acde5360bf7f
Nikita Makhotin <makhotin.07@gmail.com>
2022-11-15 23:34:36 +0300

    5 rew

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,8 +6,6 @@ from posts.models import Comment, Group, Post
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
-    post = serializers.SlugRelatedField(read_only=True,
-                                        slug_field='pk')
 
     class Meta:
         model = Comment
